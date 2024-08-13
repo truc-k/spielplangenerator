6bfccffc27872cc773affe6289d52da3ea04cc6c
Curt Kindler <curtkindler@gmx.de>
2024-08-13 10:41:11 +0200

    Test mit Leistungsspielern und Namen

--- javascript-code/zufallsspielplan.js
+++ javascript-code/zufallsspielplan.js
@@ -3,12 +3,15 @@ function spielernamenWahl() {
     var auswahlOhneName = document.getElementById("ohne-namen");
     var listeNamen = document.getElementById("namen-spieler");
     var zahlSpieler = document.getElementById("anzahl-spieler-ohne-namen");
+    var leistungsspielerAuswahl = document.getElementById("leistungsspieler-auswahl");
 
     if (auswahlMitName.checked == true) {
         listeNamen.style.display = "block";
         zahlSpieler.style.display = "none";
+        leistungsspielerAuswahl.style.display = "block";
     } else if (auswahlOhneName.checked == true) {
-        zahlSpieler.style.display = "block";
         listeNamen.style.display = "none";
+        zahlSpieler.style.display = "block";
+        leistungsspielerAuswahl.style.display = "none";
     }
 }
