b055580c45b9761f9df0c1cb57a0bff7561f1480
Curt Kindler <curtkindler@gmx.de>
2024-08-14 20:50:33 +0200

    anpassen der Turniereinstellungsübersicht

--- javascript-code/zufallsspielplan.js
+++ javascript-code/zufallsspielplan.js
@@ -1,20 +1,52 @@
 function spielernamenWahl() {
-    var auswahlMitName = document.getElementById("mit-namen");
-    var auswahlOhneName = document.getElementById("ohne-namen");
-    var listeSpieler = document.getElementById("namen-spieler");
-    var zahlSpieler = document.getElementById("anzahl-spieler-ohne-namen");
-    var listeLeistungsspieler = document.getElementById("namen-leistungspieler");
-    var anzahlLeistungsspieler = document.getElementById("leistungsspieler-anzahl");
+    //Hier wird zwischen der Eingabe der Spielernamen und Spieleranzahl gewählt und entsprechend werden die Test- bzw. Zahlfelder angepasst.
 
-    if (auswahlMitName.checked == true) { //Einstellung für Turnier mit Namen
-        listeSpieler.style.display = "block";
-        zahlSpieler.style.display = "none";
-        listeLeistungsspieler.style.display = "block";
-        anzahlLeistungsspieler.style.display = "none";
-    } else if (auswahlOhneName.checked == true) { //Einstellungen für ein Turnier ohne Namen
-        listeSpieler.style.display = "none";
-        zahlSpieler.style.display = "block";
-        listeLeistungsspieler.style.display = "none";
-        anzahlLeistungsspieler.style.display = "block";
+    //Deklaration der Variablen
+    //Variablen Turnier mit Spielernamen
+    let auswahlMitName = document.getElementById("mit-namen"); //Auswahlpunkt für Turnier mit Spielernamen
+    let textfeldNamenSpieler = document.getElementById("namen-spieler"); //Abfragefeld für Spielernamen
+    let textfeldNamenLeistungsspieler = document.getElementById("namen-leistungspieler"); //Abfragefeld für Leistungsspielernamen
+
+    //Variablen Turnier ohne Spielernamen
+    let auswahlOhneName = document.getElementById("ohne-namen"); //Auswahlpunkt für Turnier ohne Spielernamen
+    let zahlfeldAnzahlSpieler = document.getElementById("anzahl-spieler-ohne-namen"); //Abfragefeld für Spieleranzahl
+    let zahlfeldAnzahlLeistungsspieler = document.getElementById("anzahl-leistungsspieler"); //Abfragefeld für Leistungsspieleranzahl
+
+    //if-Bedingung prüft, ob Auswahlfeld ausgewählt wird und ob die Felder für die Anzahl der Spieler und Leistungsspieler beschrieben sind
+    if (auswahlMitName.checked == true && zahlfeldAnzahlSpieler.value == "" && zahlfeldAnzahlLeistungsspieler.value == "") { //Einstellung für Turnier mit Namen
+        //Spieler- und Leistungsspielernamen können eingetragen werden, Zahlfelder sind nicht sichtbar
+        textfeldNamenSpieler.style.display = "block";
+        textfeldNamenLeistungsspieler.style.display = "block";
+        zahlfeldAnzahlSpieler.style.display = "none";
+        zahlfeldAnzahlLeistungsspieler.style.display = "none";
+
+    //else-if-Bedingung prüft, ob Auswahlfeld ausgewählt wird und ob die Felder für die Spieler- und Leistungsspielernamen beschrieben sind
+    } else if (auswahlOhneName.checked == true && textfeldNamenSpieler.value == "" && textfeldNamenLeistungsspieler.value == "") { //Einstellungen für ein Turnier ohne Namen
+        //Zahlfelder für Spieler- und Leistungsspieleranzahl können eingetragen werden, Namensfelder sind nicht sichtbar
+        textfeldNamenSpieler.style.display = "none";
+        textfeldNamenLeistungsspieler.style.display = "none";
+        zahlfeldAnzahlSpieler.style.display = "block";
+        zahlfeldAnzahlLeistungsspieler.style.display = "block";
     }
+}
+
+function initialisierungTurnier() {
+    //Hier werden die Turniereinstellungen gespeichert, um später wieder abgerufen werden zu können.
+    
+    //Deklaration der Variablen
+    //allgemeine Variablen
+    let teamgroeße = document.getElementById("teamgroeße"); //Abfragefeld für Spieler pro Team
+    let teamanzahl = document.getElementById("teamanzahl"); //Abfragefeld für Teams pro Spielfeld
+    let spielfeldAnzahl = document.getElementById("spielfeld-anzahl"); //Abfragefeld für optionale Beschränkung der Spielfeldanzahl
+
+    //Variablen Turnier mit Spielernamen
+    let auswahlMitName = document.getElementById("mit-namen"); //Auswahlpunkt für Turnier mit Spielernamen
+    let textfeldNamenSpieler = document.getElementById("namen-spieler"); //Abfragefeld für Spielernamen
+    let textfeldNamenLeistungsspieler = document.getElementById("namen-leistungspieler"); //Abfragefeld für Leistungsspielernamen
+
+    //Variablen Turnier ohne Spielernamen
+    let auswahlOhneName = document.getElementById("ohne-namen"); //Auswahlpunkt für Turnier ohne Spielernamen
+    let zahlfeldAnzahlSpieler = document.getElementById("anzahl-spieler-ohne-namen"); //Abfragefeld für Spieleranzahl
+    let zahlfeldAnzahlLeistungsspieler = document.getElementById("anzahl-leistungsspieler"); //Abfragefeld für Leistungsspieleranzahl
+
 }
